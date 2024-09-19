aa363e6dd4c787478cb60c05aa65eb88c4041b4e
MPSC1977 <mp.carvalho1977@gmail.com>
2024-09-19 19:10:58 -0300

    diretório da aula 01

---
+++ Aula01/main.py
@@ -0,0 +1,46 @@
+# print("Hello World!")
+# print('Marcos Paulo')
+# string - cadeia de caracteres
+# int - números inteiros
+# float - números reais
+# boll - True | False
+# CTRL + ALT + N - rodar
+# +
+# -
+# *
+# /
+# % - módulo (resto da divisão)
+# // - (parte inteira da divisão)
+
+# n1 = 5
+# n2 = 2
+
+# resultado = (n1 // n2)
+
+# print(resultado)
+
+print('Hello World')
+Assunto = 'Algoritmos em Python'
+print(Assunto)
+nome = 'Patrick'
+print('Ola', nome)
+cor = 'azul'
+Identificacao = 'Marcos'
+Prof = 'Tecnico'
+Resid = 'Sao Goncalo'
+print(Identificacao, cor)
+print('Ola,', Identificacao, 'a sua profissao e', Prof, 'e voce mora em', Resid)
+usuario = input('Informe seu nome: ')
+ano_nascimento = int(input('Informe seu ano de nascimento: '))
+altura = float(input('Informe sua altura: '))
+
+idade = 2024 - ano_nascimento
+
+print(f'Seu nome é {usuario}, o seu ano de nascimento é {ano_nascimento} e sua altura é {altura}')
+print(f'A sua idade é {idade}')
+
+# ano_nascim = 1981
+# ano_atual = 2024
+
+# idade = ano_atual - ano_nascim
+# print(idade)
